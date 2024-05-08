ee3a0ce8d226b0565cea132ed29abc8433ae9827
Tanguy <tanguymorel.pro@gmail.com>
2024-05-08 17:14:20 +0200

    add: axios for datas

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios';
+
+interface Pokemon {
+  name: string
+  url: string
+}
+
+interface singlePokemon {
+  id: number
+  name: string
+  sprites: {
+    front_default: string
+  }
+  types: {
+    type: {
+      name: string
+    }
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [pokemons, setPokemons] = useState<singlePokemon[]>([]);
+
+  useEffect(() => {
+    const getPokemons = async () => {
+      const API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=50&offset=0'; 
+      const res = await axios.get(API_URL)
+  
+      // Utilisation de Promise.all pour attendre toutes les requêtes individuelles
+      const pokemonDataPromises = res.data.results.map(async (pokemon: Pokemon) => {
+        const singlePokemonResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        return singlePokemonResponse.data;
+      });
+
+      // Attendre que toutes les requêtes soient terminées
+      const pokemonData = await Promise.all(pokemonDataPromises);
+
+      setPokemons(pokemonData);
+    }
+  
+    getPokemons();
+  }, []);
+  
+  console.log(pokemons);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <section className='app-section'>
+      Pokemon
+    </section>
   )
 }
 
